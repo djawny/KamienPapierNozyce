2feff7a98ed15d2424a6142a5984dd25e883d739
djawny <djawny@op.pl>
2017-02-12 22:43:52 +0100

    zmiana

--- app/src/main/java/com/example/daniel/kamienpapiernozyce/MainActivity.java
+++ app/src/main/java/com/example/daniel/kamienpapiernozyce/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        setListeners();
-        init();
+//        ButterKnife.bind(this);
+//        setListeners();
+//        init();
     }
 
     private void setListeners() {
