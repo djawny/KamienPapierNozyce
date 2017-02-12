fc5e7cb6f8b2813c40e0ede81f04d520d4d0c1b9
djawny <djawny@op.pl>
2017-02-12 01:02:54 +0100

    zmiana

--- app/src/main/java/com/example/daniel/kamienpapiernozyce/MainActivity.java
+++ app/src/main/java/com/example/daniel/kamienpapiernozyce/MainActivity.java
@@ -2,8 +2,34 @@ package com.example.daniel.kamienpapiernozyce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.computerChoiceImageView)
+    private ImageView computerChoiceImageView;
+
+    @BindView(R.id.gamerChoiceImageView)
+    private ImageView gamerChoiceImageView;
+
+    @BindView(R.id.gamerWinsTextView)
+    private TextView gamerWinsTextView;
+
+    @BindView(R.id.computerWinsTextView)
+    private TextView computerWinsTextView;
+
+    @BindView(R.id.paperButton)
+    private Button paperButton;
+
+    @BindView(R.id.scissorsButton)
+    private Button scissorsButton;
+
+    @BindView(R.id.rockButton)
+    private Button rockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
